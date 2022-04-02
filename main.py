18e608530655d294fe24d997f3104463f89d4c85
GabrielRak <gabrielblazejrak@gmail.com>
2022-04-02 11:53:30 +0200

    Structural Version v1

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.ceneo.pl/68112285#tab=reviews'
+
 
 def main():
-    pass
+    page = requests.get(URL)
+
+    bs = BeautifulSoup(page.content, 'html.parser')
+
+    opinion = bs.find( 'div', class_="user-post")
+    content = opinion.find( 'div', class_="user-post__text").get_text().strip()
+    print(content)
+
+
 
 if __name__ == "__main__":
     main()
