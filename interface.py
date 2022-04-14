8d34e6b308a9db54ad3f9f88def6389d7d7d0c82
GabrielRak <gabrielblazejrak@gmail.com>
2022-04-14 21:41:53 +0200

    Merge from procedural App

---
+++ interface.py
@@ -0,0 +1,82 @@
+import os 
+import json
+from reader import Reader
+from analyst import Analyst
+from functionalities import display_message
+
+class Interface():
+    
+    def __init__(self,title):
+        self.title = title
+
+    def run(self):
+        self.display_menu()
+
+    def display_menu(self):
+
+        while True:
+
+            os.system("clear")
+
+            print(f'''
+{self.title} [Menu]
+[0].Quit
+[1].Check an item 
+[2].Show subscribed items  
+            ''')
+            
+            choice = input("Your choice:")
+            
+            try:
+                if choice == '0':
+                    break
+                elif choice == '1':
+                    reader = Reader()
+                    item_url = input("Item url:")
+                    data = reader.Read_Reviews(item_url,True)
+                elif choice == '2':
+                    
+                    id = 0
+                    subscribed_items = {}
+                    
+                    for name in os.listdir("./Data"):
+                        subscribed_items[str(id)] = name
+                        print(f'{id}:{name}')
+                        id+=1
+                        
+                    
+                    choice = input("Your choice:")
+                    try:
+                        path = "./Data/" + subscribed_items[str(choice)] + "/" + subscribed_items[str(choice)] +"_raw_data.json"
+                    except KeyError:
+                        display_message(f'U can choose only [1-{id-1}] items')
+                        self.display_menu()
+                    os.system("clear")
+
+                    print(f'''
+Your item is {subscribed_items[str(choice)]}
+What do you want to do with it
+1.Show average data 
+2.Render chart about it recommendations
+                    ''')
+
+                    choice = input("Your choice:")
+
+                    if choice == '1': 
+                        analyst = Analyst()
+                        analyst.show_data(path)
+                    elif choice == '2':
+                        analyst = Analyst()
+                        analyst.analyze(path)
+                    else:
+                        display_message("There is just 2 options")
+                    display_message('')
+                else: 
+                    display_message("Choose another option")
+            except ValueError:
+                    self.display_menu("Choose another option")
+        
+
+
+
+
--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = 'https://www.ceneo.pl/68112285#tab=reviews'
-
+from interface import Interface
 
 def main():
-    page = requests.get(URL)
 
-    bs = BeautifulSoup(page.content, 'html.parser')
-
-    COUNTER = 1
-
-    for opinion in bs.find_all('div',class_="user-post"):
-        content = opinion.find('div',class_="user-post__text").get_text().strip()
-        print(f'{COUNTER}.{content}')
-        COUNTER+=1 
-        if COUNTER > 10:break
+    app = Interface("Ceneo Scrapper")
+    app.run()
 
 if __name__ == "__main__":
     main()
